4fcf35d60278e14f5a10d7ffae661c4c2faec8cb
Wout Hiemstra <wout.hiemstra@hotmail.com>
2023-12-16 21:36:41 +0100

    day one

--- day-1/src/lib/index.ts
+++ day-1/src/lib/index.ts
@@ -1,1 +1,50 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { browser } from "$app/environment";
+import { get, writable } from "svelte/store";
+
+export const listChildren = writable<Child[]>([]);
+export const niceList = writable<Child[]>([]);
+export const badList = writable<Child[]>([]);
+
+type Child = {
+    name: string;
+    tally: number;
+};
+
+export const getChildren = async () => {
+    console.log('in funciton');
+    if (browser) {
+        const localStorageString: string | null = localStorage.getItem('names');
+        if (localStorageString === null || undefined) {
+            // Fetch data.
+            const respone = await fetch('https://advent.sveltesociety.dev/data/2023/day-one.json');
+
+            // Get response as json, and set in in local storage if none.
+            const data = await respone.json();
+            localStorage.setItem('names', JSON.stringify(data));
+            listChildren.set(data);
+        } else {
+            let typedList: Child[] = JSON.parse(localStorageString);
+            niceList.set(typedList.filter(item => item.tally > 0));
+            badList.set(typedList.filter(item => item.tally < 0));
+            listChildren.set(typedList);
+        }
+    }
+};
+
+export const addNew = (name: string, tally: number) => {
+    let newChild: Child = {
+        name: name,
+        tally: tally,
+    };
+
+    if (tally > 0) {
+        let curr: Child[] = get(niceList);
+        curr.push(newChild);
+        niceList.set(curr);
+    } else {
+        let curr: Child[] = get(badList);
+        curr.push(newChild);
+        badList.set(curr);
+    }
+};
+
--- day-1/src/routes/+page.svelte
+++ day-1/src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { getChildren, addNew } from '$lib';
+	import { niceList, badList } from '$lib/index';
+
+	getChildren();
+	let newName: string;
+	let score: number;
+</script>
+
+<div class="border border-black p-5 m-5 flex justify-around">
+	<input class="p-2 border border-purple-300" type="text" bind:value={newName} placeholder="Name" />
+	<input
+		class="p-2 border border-purple-300"
+		type="number"
+		bind:value={score}
+		placeholder="Score"
+	/>
+	<button on:click={() => addNew(newName, score)}>Submit</button>
+</div>
+
+<div class="flex justify-between p-5">
+	<div class="flex flex-col p-2 border border-gray-200">
+		<h1 class="text-3xl">Nice List</h1>
+		{#each $niceList as child}
+			<div class="border m-2 w-56 p-2 flex flex-row">
+				{child.name}
+				<span class="mx-2">|</span>
+				{child.tally}
+				<br />
+			</div>
+		{/each}
+	</div>
+
+	<div class="flex flex-col p-2 border border-gray-200">
+		<h1 class="text-3xl">Bad List</h1>
+		{#each $badList as child}
+			<div class="border m-2 w-56 p-2 flex flex-row">
+				{child.name}
+				<span class="mx-2">|</span>
+				{child.tally}
+				<br />
+			</div>
+		{/each}
+	</div>
+</div>
